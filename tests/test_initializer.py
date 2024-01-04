132344a8e130855c45886a92b75ba34a4a4025d4
Torsten Wylegala <mail@twyleg.de>
2024-01-05 00:42:50 +0100

    Added template_project_cpp and template_project_cpp_qt_qml to unit tests

--- tests/test_initializer.py
+++ tests/test_initializer.py
@@ -6,6 +6,8 @@ import shutil
 import logging
 
 from pathlib import Path
+from typing import List
+
 from template_project_utils.initializer import init_template
 
 FORMAT = "[%(asctime)s][%(levelname)s][%(name)s]: %(message)s"
@@ -18,47 +20,93 @@ FILE_DIR = Path(__file__).parent
 
 
 class TemplateInitializeTestCase(unittest.TestCase):
+    def __init__(self, method_name: str, template_project_name: str):
+        super().__init__(method_name)
+        self.template_project_name = template_project_name
+        self.template_project_path = FILE_DIR / f"../external/{template_project_name}"
+
     @classmethod
     def prepare_output_directory(cls) -> Path:
         tmp_dir = tempfile.mkdtemp()
         return Path(tmp_dir)
 
+    def prepare_template_project(self, dst_dir: Path) -> Path:
+        dst_path = dst_dir / self.template_project_name
+        shutil.copytree(self.template_project_path, dst_path, symlinks=False)
+        return dst_path
+
+    def expect_no_occurrences_of_keywords(self, keywords: List[str]):
+        dir_name_count = 0
+        file_content_count = 0
+
+        for path in self.test_project_path.rglob("*"):
+            if path.is_relative_to(self.test_project_path / ".git/"):
+                pass  # Ignore
+            elif path.is_relative_to(self.test_project_path / "venv/"):
+                pass  # Ignore
+            elif path.is_dir():
+                for keyword in keywords:
+                    if keyword in str(path.relative_to(self.test_project_path)):
+                        dir_name_count += 1
+                        logging.debug("Dir path containing keyword '%s': %s", keyword, path)
+            elif path.is_file():
+                try:
+                    content = path.read_text()
+                    for keyword in keywords:
+                        count = content.count(keyword)
+                        file_content_count += count
+                        if count:
+                            logging.debug("File containing keyword '%s': %s", keyword, path)
+                except UnicodeDecodeError as e:
+                    pass
+        self.assertEqual(0, dir_name_count)
+        self.assertEqual(0, file_content_count)
+
     def setUp(self) -> None:
         logging.basicConfig(stream=sys.stdout, format=FORMAT, level=logging.DEBUG)
         self.output_dir_path = self.prepare_output_directory()
-        logging.info("Tmp working dir: %s", self.output_dir_path)
+        self.test_project_path = self.prepare_template_project(self.output_dir_path)
+        logging.info("Tmp working dir: %s", self.template_project_path)
 
     def tearDown(self):
         logging.shutdown()
         print("")
 
 
-class PythonTemplateInitializeTestCase(TemplateInitializeTestCase):
-    TEMPLATE_PROJECT_PYTHON_DIR = FILE_DIR / "../external/template_project_python"
-
-    @classmethod
-    def prepare_template_project_python(cls, dst_dir: Path) -> Path:
-        dst_path = dst_dir / "template_project_python"
-        shutil.copytree(cls.TEMPLATE_PROJECT_PYTHON_DIR, dst_path)
-        return dst_path
+class CppTemplateInitializeTestCase(TemplateInitializeTestCase):
+    def __init__(self, method_name="runTest"):
+        super().__init__(method_name, "template_project_cpp")
 
     def test_TemplateCreated_Initialize_Success(self):
-        test_project_path = self.prepare_template_project_python(self.output_dir_path)
-        init_template(test_project_path / "template_config.yaml", "target_string", dry_run=False)
+        init_template(self.test_project_path / "template_config.yaml", "target_string", dry_run=False)
+        self.expect_no_occurrences_of_keywords(["template_project_cpp"])
+
+
+class CppQtQmlTemplateInitializeTestCase(TemplateInitializeTestCase):
+    def __init__(self, method_name="runTest"):
+        super().__init__(method_name, "template_project_cpp_qt_qml")
+
+    def test_TemplateCreated_Initialize_Success(self):
+        init_template(self.test_project_path / "template_config.yaml", "target_string", dry_run=False)
+        self.expect_no_occurrences_of_keywords(["template_project_cpp_qt_qml"])
+
+
+class PythonTemplateInitializeTestCase(TemplateInitializeTestCase):
+    def __init__(self, method_name="runTest"):
+        super().__init__(method_name, "template_project_python")
+
+    def test_TemplateCreated_Initialize_Success(self):
+        init_template(self.test_project_path / "template_config.yaml", "target_string", dry_run=False)
+        self.expect_no_occurrences_of_keywords(["template_project_python"])
 
 
 class KicadTemplateInitializeTestCase(TemplateInitializeTestCase):
-    TEMPLATE_PROJECT_KICAD_DIR = FILE_DIR / "../external/template_project_kicad"
-
-    @classmethod
-    def prepare_template_project_kicad(cls, dst_dir: Path) -> Path:
-        dst_path = dst_dir / "template_project_kicad"
-        shutil.copytree(cls.TEMPLATE_PROJECT_KICAD_DIR, dst_path, symlinks=False)
-        return dst_path
+    def __init__(self, method_name="runTest"):
+        super().__init__(method_name, "template_project_kicad")
 
     def test_TemplateCreated_Initialize_Success(self):
-        test_project_path = self.prepare_template_project_kicad(self.output_dir_path)
-        init_template(test_project_path / "template_config.yaml", "target_string", dry_run=False)
+        init_template(self.test_project_path / "template_config.yaml", "target_string", dry_run=False)
+        self.expect_no_occurrences_of_keywords(["template_project_kicad"])
 
 
 if __name__ == "__main__":
